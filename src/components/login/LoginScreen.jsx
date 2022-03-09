ca10a22beb8729f89666ff9088586c97c2dd13fe
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-09 14:44:20 -0500

    Improving login

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -1,26 +1,46 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
+
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import useLoginScreen from 'components/hooks/useLoginScreen';
 import Form from 'components/ui/Form';
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
 
 const theme = createTheme();
 
 const LoginScreen = () => {
     const { fields } = useLoginScreen();
+    const backgroundProperties = {
+        backgroundImage:
+            'url(https://www.educaciontrespuntocero.com/wp-content/uploads/2019/03/cursos-musica.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    };
 
     return (
         <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
+            <Grid container component="main" sx={{ height: '100vh' }}>
                 <CssBaseline />
-                <Form
-                    fields={fields}
-                    title={'Log In'}
-                    aLink={"Don't have an account? Sign Up"}
-                    href={'#'}
-                />
-            </Container>
+                <Grid item xs={false} sm={4} md={7} sx={backgroundProperties} />
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={5}
+                    component={Paper}
+                    elevation={6}
+                    square
+                >
+                    <Form
+                        fields={fields}
+                        title={'Log In'}
+                        aLink={"Don't have an account? Sign Up"}
+                        href={'#'}
+                    />
+                </Grid>
+            </Grid>
         </ThemeProvider>
     );
 };
