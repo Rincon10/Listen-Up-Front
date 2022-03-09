d9ae24ec69ccce99027c8ff032a0e6604a776c20
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-09 16:52:19 -0500

    basic operations to Login

--- src/components/services/userApiClient.js
+++ src/components/services/userApiClient.js
@@ -1,4 +1,29 @@
 export const userApiclient = (() => {
-    const URL = 'SOME_URL';
-    return {};
+    const URL = 'http://localhost:8080';
+
+    const myHeader = new Headers();
+    myHeader.set('Content-Type', 'application/json');
+    myHeader.set('Access-Control-Allow-Origin', '*');
+
+    return {
+        postUser: async userDTO => {
+            const response = await fetch(`${URL}/user`, {
+                method: 'POST',
+                headers: myHeader,
+                body: JSON.stringify(userDTO),
+            });
+            if (!response.ok) throw new Error('The response failed');
+            return response.json();
+        },
+
+        getToken: async loginDTO => {
+            const response = await fetch(`${URL}/auth`, {
+                method: 'POST',
+                headers: myHeader,
+                body: JSON.stringify(loginDTO),
+            });
+            if (!response.ok) throw new Error('The response failed');
+            return response.json();
+        },
+    };
 })();
