e77724a66ee02c7f394e44fc193cd7a79cb75baa
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-09 14:03:03 -0500

    Form Component without hooks

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -1,7 +1,64 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextFieldContainer from 'components/ui/TextFieldContainer';
 
-const Form = () => {
-    return <div>Some Form</div>;
+import '../../css/form.css';
+
+const Form = ({ title, fields, aLink, href }) => {
+    return (
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{ m: 1 }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                {title}
+            </Typography>
+            <Box
+                component="form"
+                /* onSubmit={handleSubmit} */
+                noValidate
+                sx={{ mt: 1 }}
+            >
+                <TextFieldContainer textFieldItems={fields} />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Log In
+                </Button>
+                <Grid container>
+                    <Grid item>
+                        <Link href={href} variant="body2">
+                            {aLink}
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+    );
+};
+
+Form.propTypes = {
+    aLink: PropTypes.string,
+    fields: PropTypes.array,
+    href: PropTypes.string,
+    title: PropTypes.string,
 };
 
 export default Form;
